3089b1960c97baaa193ff54d8a87ee07a9b6a94a
IBM Abdulsalam <ibmabdulsalam@gmail.com>
2024-11-01 23:07:00 +0100

    latest

--- backend/thumbs/models.py
+++ backend/thumbs/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from users.models import CustomUsers
 from utils import  image_resize
 import uuid
 from product.models import Product
@@ -10,22 +9,17 @@ User = get_user_model()
 def product_image_path(instance, filename):
     return '/'.join([str(instance.product.id), filename])
 
-
-from django.db import models
-
-class ImageField(models.ImageField):
-    def value_to_string(self, obj): # obj is Model instance, in this case, obj is 'Class'
-        return obj.img.url # not return self.url
-
-
 class ProductImage(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     product = models.ForeignKey(Product, related_name="images", on_delete=models.CASCADE)
-    img = ImageField(_("Upload Image"), upload_to=product_image_path)
+    img = models.ImageField(_("Upload Image"), upload_to=product_image_path,)
     uploaded_on = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.product.name
+    
+    def get_image(self):
+        return self.img.url
     
     def save(self, *args, **kwargs):
         if self.img.file:
--- backend/thumbs/serializers.py
+++ backend/thumbs/serializers.py
@@ -2,6 +2,10 @@ from rest_framework import serializers
 from .models import ProductImage
 
 class ProductImageSerializer(serializers.ModelSerializer):
+    img = serializers.ImageField(
+            max_length=None, use_url=True
+        )
     class Meta:
         model = ProductImage
-        fields = ("id", "img", "product")
+        fields = ("id", "img", "product", "get_image")
+        read_only_field = ("get_image",)
