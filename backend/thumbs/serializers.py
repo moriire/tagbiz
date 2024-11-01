1a0f2bbf0f73420f9b5adf3824823dad1afa0ca1
IBM Abdulsalam <ibmabdulsalam@gmail.com>
2024-11-01 23:26:01 +0100

    latest

--- backend/thumbs/serializers.py
+++ backend/thumbs/serializers.py
@@ -6,9 +6,7 @@ class ProductImageSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = ProductImage
-        fields = ("id", "img", "product", "image_url")
-        read_only_field = ("image_url",)
+        fields = ("id", "img", "product")#, "image_url")
+        #read_only_field = ("image_url",)
 
-    def get_image_url(self, obj):
-        #request = self.context.get("request")
-        return self.build_url_field(ProductImage, "img")
+    
