20db313ea6593c5b00813936e837cb3180ae6369
IBM Abdulsalam <ibmabdulsalam@gmail.com>
2024-11-01 23:10:19 +0100

    latest

--- backend/thumbs/serializers.py
+++ backend/thumbs/serializers.py
@@ -2,10 +2,12 @@ from rest_framework import serializers
 from .models import ProductImage
 
 class ProductImageSerializer(serializers.ModelSerializer):
-    img = serializers.ImageField(
-            max_length=None, use_url=True
-        )
+    
     class Meta:
         model = ProductImage
-        fields = ("id", "img", "product", "get_image")
-        read_only_field = ("get_image",)
+        fields = ("id", "img", "product", "get_image_url")
+        read_only_field = ("get_image_url",)
+
+    def get_image_url(self, obj):
+        request = self.context.get("request")
+        return request.build_absolute_uri(obj.img.url)
